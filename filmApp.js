3dfe64fa85e22a671af3894c5c72b8ba1dd5fc6d
Andrushka <Andrusha.kolmakov@gmail.com>
2022-07-04 23:04:55 +0300

    create methods in app and add favicon

--- filmApp.js
+++ filmApp.js
@@ -1,70 +1,143 @@
 // пишем приложение фильмов
 
-let personalMoveDataBase;
 
-function showFilm(){
+
 
         personalMoveDataBase = {
         count: 0,
         movies: {},
         actors: {},
         genres: [],
-        privat: true
+        privat: true,
+        showMyDataBase:  function(){
+            // Настройки приватности базы данных
+            if(personalMoveDataBase.privat){
+                console.log(personalMoveDataBase);
+            } else {
+                console.log("скрыто настройками приватности")
+            }  
+        },
+
+        toggleWisibleMyDataBase: function(){
+            // флаг приватности
+            if(personalMoveDataBase.privat){
+                personalMoveDataBase.privat = false
+            } else{
+                personalMoveDataBase.privat = true
+            }
+        },
+
+        filmCounter: function(){
+            // счетчик фильмов
+            personalMoveDataBase.count = +prompt("сколько фильмов вы посмотрели?");
+            if(personalMoveDataBase.count > 10 && personalMoveDataBase.privat === true){
+                console.log("вы киноман")
+            }else if(personalMoveDataBase.count <=10  && personalMoveDataBase.privat === true){
+                console.log("маловато фильмов")
+            } else if(personalMoveDataBase.count != typeof(Number)  && personalMoveDataBase.privat === true){
+                console.log("количество - не цифра")
+                
+            }
+        },
+
+        moviesCasting: function(){
+            // вопросы о фильмах и оценки
+            let movies = {};
+        for(let i = 0; i<2; i++){
+                let film = prompt("Какой фильм вы смотрели?")
+            let raiting = +prompt("Какую оценку поставите? (1-10)")
+            // personalMoveDataBase.count++
+            if(film != null && raiting != null && film != '' && raiting != '' && film.length <=10 && raiting <= 10){
+                personalMoveDataBase.movies[film] = raiting; 
+            }else {
+                console.error("пользователь долбоеб")
+                alert("ваш результат не валиден, пройдите опрос еще раз")
+                i--; 
+            }
+            }
+        },
+
+        writeYourGenres: function(){
+            // спрашиваем любимые жанры через запятую с помощью split()
+            // и пересчитываем через forEach() с выводом в консоль
+            for(let i=1; i<2; i++){
+                let genre = prompt(`Введите ваши любимые жанры через Запятую`).toLowerCase();
+                if(genre == "" || genre == null){
+                    console.log("вы ввели неверное значение")
+                    i--;
+                }  else {
+                   personalMoveDataBase.genres = genre.split(", ")
+                }
+            }
+            personalMoveDataBase.genres.forEach((el, i, array)=>{
+                console.log(`любимый жанр ${i+1} это ${el}`)
+            })
+        },
     };
 
 
+        
 
 
-    function showMyDataBase(arg){
-        if(arg === true){
-            console.log(personalMoveDataBase);
-        } else {
-            console.log("скрыто настройками приватности")
-        }
-    };
 
-       function filmCounter (){
-        personalMoveDataBase.count = +prompt("сколько фильмов вы посмотрели?");
-        if(personalMoveDataBase.count > 10 && personalMoveDataBase.privat === true){
-            console.log("вы киноман")
-        }else if(personalMoveDataBase.count <=10  && personalMoveDataBase.privat === true){
-            console.log("маловато фильмов")
-        } else if(personalMoveDataBase.count != typeof(Number)  && personalMoveDataBase.privat === true){
-            console.log("количество - не цифра")
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // function showMyDataBase(arg){
+    //     if(arg === true){
+    //         console.log(personalMoveDataBase);
+    //     } else {
+    //         console.log("скрыто настройками приватности")
+    //     }
+    // };
+
+    //    function filmCounter (){
+    //     personalMoveDataBase.count = +prompt("сколько фильмов вы посмотрели?");
+    //     if(personalMoveDataBase.count > 10 && personalMoveDataBase.privat === true){
+    //         console.log("вы киноман")
+    //     }else if(personalMoveDataBase.count <=10  && personalMoveDataBase.privat === true){
+    //         console.log("маловато фильмов")
+    //     } else if(personalMoveDataBase.count != typeof(Number)  && personalMoveDataBase.privat === true){
+    //         console.log("количество - не цифра")
             
-        }
-    };
+    //     }
+    // };
 
       
-    function moviesCasting(){
-        let movies = {};
-    for(let i = 0; i<2; i++){
-            let film = prompt("Какой фильм вы смотрели?")
-        let raiting = +prompt("Какую оценку поставите? (1-10)")
-        // personalMoveDataBase.count++
-        if(film != null && raiting != null && film != '' && raiting != '' && film.length <=10 && raiting <= 10){
-            personalMoveDataBase.movies[film] = raiting; 
-        }else {
-            console.error("пользователь долбоеб")
-            alert("ваш результат не валиден, пройдите опрос еще раз")
-            i--; 
-        }
-        }
+    // function moviesCasting(){
+    //     let movies = {};
+    // for(let i = 0; i<2; i++){
+    //         let film = prompt("Какой фильм вы смотрели?")
+    //     let raiting = +prompt("Какую оценку поставите? (1-10)")
+    //     // personalMoveDataBase.count++
+    //     if(film != null && raiting != null && film != '' && raiting != '' && film.length <=10 && raiting <= 10){
+    //         personalMoveDataBase.movies[film] = raiting; 
+    //     }else {
+    //         console.error("пользователь долбоеб")
+    //         alert("ваш результат не валиден, пройдите опрос еще раз")
+    //         i--; 
+    //     }
+    //     }
 
-    };
+    // };
 
-    function writeYourGenres(){
-        for(let i=1; i<=3; i++){
-            personalMoveDataBase.genres.push(prompt(`Ваш любимый жанр под номером${i}`))
-        }
-    };
+    // function writeYourGenres(){
+    //     for(let i=1; i<=3; i++){
+    //         personalMoveDataBase.genres.push(prompt(`Ваш любимый жанр под номером${i}`))
+    //     }
+    // };
 
-    showMyDataBase(personalMoveDataBase.privat)
-    filmCounter()
-    moviesCasting()
-    writeYourGenres()
-
-};
-
-showFilm()
-        
+    // showMyDataBase(personalMoveDataBase.privat)
+    // filmCounter()
+    // moviesCasting()
+    // writeYourGenres()
